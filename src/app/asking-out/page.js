2eeded49f83d2da69ce4327a55244db2be856b2b
Atqiya Haydar <atqiyahaydar15@gmail.com>
2025-09-01 00:01:12 +0700

    feat

--- src/app/asking-out/page.js
+++ src/app/asking-out/page.js
@@ -18,6 +18,7 @@ export default function AskingOut() {
                 <p className="text-xs">Paris Van Java</p>
             </div>
             <p className="text-xs text-center text-gray-600 pt-8">P.S karena situasi nya lagi ga baik, max 17.30 udah pulang, tanggalnya bisa nyesuain kamu kapan kosongnya.</p>
+            <p className="text-xs text-center text-gray-600 pt-4">but if you don't want to go, you can say no, I will understand.</p>
         </div>
     )
 }
