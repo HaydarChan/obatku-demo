6e95ecbab8f4b11c81b501f51aa90b4760a8000d
Atqiya Haydar <atqiyahaydar15@gmail.com>
2025-09-01 16:03:35 +0700

    feaT

--- src/app/asking-out/page.js
+++ src/app/asking-out/page.js
@@ -17,8 +17,8 @@ export default function AskingOut() {
                 <MapPin className="w-4 h-4" />
                 <p className="text-xs">Paris Van Java & Theo's Wife Lois</p>
             </div>
-            <p className="text-xs text-center text-gray-600 pt-8">P.S karena situasi nya lagi ga baik, max 17.30 udah pulang, tanggalnya bisa nyesuain kamu kapan kosongnya.</p>
-            <p className="text-xs text-center text-gray-600 pt-4">but if you don't want to go, you can say no, I will understand.</p>
+            <p className="text-xs text-center text-gray-600 pt-8">13.00 - 17.30 due to the condition</p>
+            <p className="text-xs text-center text-gray-600 pt-4">Disclaimer : this invitation is optional! feel free to reject</p>
         </div>
     )
 }
