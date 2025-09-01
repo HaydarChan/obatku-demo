5dda6456fa38eaed02f19708b81aeb358b13c646
Atqiya Haydar <atqiyahaydar15@gmail.com>
2025-09-01 14:06:54 +0700

    feat

--- src/app/asking-out/page.js
+++ src/app/asking-out/page.js
@@ -15,7 +15,7 @@ export default function AskingOut() {
             <p className="text-center font-beanie text-3xl">Would you like to go out with me?</p>
             <div className="flex gap-x-2 items-center">
                 <MapPin className="w-4 h-4" />
-                <p className="text-xs">Paris Van Java</p>
+                <p className="text-xs">Paris Van Java & Theo's Wife Lois</p>
             </div>
             <p className="text-xs text-center text-gray-600 pt-8">P.S karena situasi nya lagi ga baik, max 17.30 udah pulang, tanggalnya bisa nyesuain kamu kapan kosongnya.</p>
             <p className="text-xs text-center text-gray-600 pt-4">but if you don't want to go, you can say no, I will understand.</p>
