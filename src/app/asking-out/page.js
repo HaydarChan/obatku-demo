29fd78a955a89d9fe8f7baaf9f319a1630f1aa36
Atqiya Haydar <atqiyahaydar15@gmail.com>
2025-09-01 14:14:32 +0700

    feaT

--- src/app/asking-out/page.js
+++ src/app/asking-out/page.js
@@ -4,7 +4,7 @@ import { MapPin } from 'lucide-react';
 
 export default function AskingOut() {
     return (
-        <div className="w-full h-full md:max-w-[400px] shadow-[0px_4px_20px_0px_rgba(0,0,0,0.05)] bg-white rounded-[48px] flex flex-col items-center justify-start pt-40 p-8 gap-y-4">
+        <div className="w-full h-full md:max-w-[400px] shadow-[0px_4px_20px_0px_rgba(0,0,0,0.05)] bg-white rounded-[48px] flex flex-col items-center justify-start pt-20 p-8 gap-y-4">
              <Image 
                 src={Cat}
                 alt="Cat"
